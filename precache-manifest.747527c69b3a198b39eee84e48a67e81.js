2b6c728003f888f1e7b1376b31fa9483120006de
sathvik <sathvik@sathviks-MacBook-Pro.local>
2020-09-18 18:48:29 +0530

    Updates

---
+++ precache-manifest.747527c69b3a198b39eee84e48a67e81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e70f5a4781d1ed497ca8f08c20aaf3",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "6894affde73c37548096",
+    "url": "/me/static/css/main.27e36af3.chunk.css"
+  },
+  {
+    "revision": "dcded16be7262fbe07af",
+    "url": "/me/static/js/2.35c7a32b.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/me/static/js/2.35c7a32b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6894affde73c37548096",
+    "url": "/me/static/js/main.ffcf1745.chunk.js"
+  },
+  {
+    "revision": "5d4d3402acbc2f11c828",
+    "url": "/me/static/js/runtime-main.91277b70.js"
+  },
+  {
+    "revision": "adc64bb10cf0c6bced1159d4619ee677",
+    "url": "/me/static/media/logo.adc64bb1.png"
+  },
+  {
+    "revision": "06c0124be27faa0293eaf922103cc8ac",
+    "url": "/me/static/media/mypic.06c0124b.jpg"
+  }
+]);
